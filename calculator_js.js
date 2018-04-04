5fae5d1cb7d539b97bfd0975185b5861fab8a1bc
m.sawastian <m.sawastian@gmail.com>
2018-04-04 22:13:01 +0200

    Buttons update display field.

--- calculator_js.js
+++ calculator_js.js
@@ -1,5 +1,14 @@
 ;(function () {
     'use strict'
+    
+    var buttons = document.querySelectorAll('button');
+    var display = document.getElementById('display');
+   
+    alert(buttons.length);
 
-    alert(document.querySelectorAll('button').length);
+    for(var i = 0, l = buttons.length; i < l; i ++) {
+        buttons[i].addEventListener("click", function() {
+            display.innerHTML = this.innerHTML;
+        })
+    }
 })();
