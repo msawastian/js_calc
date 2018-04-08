082f1aadd61034e9584638d6f14e02cd6fed0d04
m.sawastian <m.sawastian@gmail.com>
2018-04-08 11:03:59 +0200

    Basic functionality.

--- calculator_js.js
+++ calculator_js.js
@@ -1,14 +1,22 @@
 ;(function () {
     'use strict'
     
-    var buttons = document.querySelectorAll('button');
+    var buttons = document.querySelectorAll('.number');
     var display = document.getElementById('display');
-   
-    alert(buttons.length);
+    var result = document.getElementById('eval');
+    var clear = document.getElementById('clear');
 
     for(var i = 0, l = buttons.length; i < l; i ++) {
         buttons[i].addEventListener("click", function() {
-            display.innerHTML = this.innerHTML;
+            display.innerHTML += this.innerHTML;
         })
     }
+
+    result.addEventListener("click", function() {
+        display.innerHTML = eval(display.innerHTML);
+    })
+
+    clear.addEventListener("click", function() {
+        display.innerHTML = '';
+    })
 })();
