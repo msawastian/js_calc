e093a4b57b428aff70ba16d0f746a17458e7203a
m.sawastian <m.sawastian@gmail.com>
2018-04-10 14:38:36 +0200

    Basic memory functionality.

--- calculator_js.js
+++ calculator_js.js
@@ -5,18 +5,41 @@
     var display = document.getElementById('display');
     var result = document.getElementById('eval');
     var clear = document.getElementById('clear');
+    var memoryPlus = document.getElementById('memPlus');
+    var memoryMinus = document.getElementById('memMinus');
+    var memoryDisplay = document.getElementById('memDisp');
+    var memoryClear = document.getElementById('memClear');
+    var memory = 0;
 
     for(var i = 0, l = buttons.length; i < l; i ++) {
-        buttons[i].addEventListener("click", function() {
+        buttons[i].addEventListener('click', function() {
             display.innerHTML += this.innerHTML;
         })
     }
 
-    result.addEventListener("click", function() {
+    result.addEventListener('click', function() {
         display.innerHTML = eval(display.innerHTML);
     })
 
-    clear.addEventListener("click", function() {
+    clear.addEventListener('click', function() {
         display.innerHTML = '';
     })
+
+    memoryPlus.addEventListener('click', function() {
+        memory = memory + parseInt(display.innerHTML);
+    })
+
+    memoryMinus.addEventListener('click', function() {
+        memory = memory - parseInt(display.innerHTML);
+    })
+
+    memoryDisplay.addEventListener('click', function() {
+        display.innerHTML = memory;
+    })
+
+    memoryClear.addEventListener('click', function() {
+        memory = 0;
+        display.innerHTML = '';
+    })
+
 })();
